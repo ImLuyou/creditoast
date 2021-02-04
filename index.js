1dca97948bfeca6f61ad46b83e2455d996bb85ce
Adrian Legaspi <adrian.luball@gmail.com>
2021-02-04 14:40:57 -0800

    Update

--- index.js
+++ index.js
@@ -2,36 +2,58 @@ class Creditoast {
 
     constructor(debug = false) {
         if (debug) {
-            console.warn('[Creditoast has been loaded]');
+            console.warn('[Creditoast has been loaded from local]');
         }
 
         this.toastTimeout = null;
         this.tmpToast = null;
     }
 
-    toast(message = '', timeoutms = 4000) {
+    toast(message = '', options = {
+        timeoutms: 4000, 
+        color: 'secondary'
+    }) {
         let main = this;
+        let className = '';
+        let timeoutms = 4000;
+
+        if (!!options.color) {
+            switch(options.color) {
+                case 'success':
+                    className = 'alert-success';
+                break;
+
+                default: 
+                    className = 'alert-secondary';
+                break;
+            }
+        }
+
+        if (!!options.timeoutms) {
+            timeoutms = options.timeoutms;
+        }
+        
         if (!(!!this.tmpToast)) {
             this.tmpToast = document.createElement('div');
             this.tmpToast.id = 'main-toast-id';
-            this.tmpToast.classList.add("alert-success");
+            this.tmpToast.classList.add(className);
             this.tmpToast.classList.add('alert');
             document.body.appendChild(this.tmpToast); 
         }
         
         this.tmpToast.innerText = message;
-    
+
         try {
             if (this.toastTimeout) {
                 clearTimeout(this.toastTimeout);
             }
-    
+
             this.toastTimeout = setTimeout(function() {
                 main.tmpToast.style.display = "none";
             }, timeoutms);
         } catch(e) {
             console.log('[UserDashboard-1/userDasboard.js] toast', e)
         }
-    
-      }
+
+    }
 }
